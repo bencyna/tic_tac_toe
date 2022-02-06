05b0f9d2f407236a6ec4e035809af1e99cc79520
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-06 16:57:58 +1030

    Add winner and prevent choosing the same spot twice

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ winning_positions = [['0, 1', '0, 2', '0, 3'], ['1, 1', '1, 2', '1, 3'], ['2, 1'
 
 player1_positions = []
 player2_positions = []
+all_chosen = []
 
 
 def init_game():
@@ -19,11 +20,30 @@ def init_game():
     print_game()
 
 
+def check_for_winner(position):
+    for winning_row in winning_positions:
+        print(set(winning_row).issubset(player1_positions))
+        if set(winning_row).issubset(player1_positions):
+            print("Winner is player 1")
+            return
+        elif set(winning_row).issubset(player2_positions):
+            print("winner is player 2")
+            return
+
+    all_chosen.append(position)
+    if len(all_chosen) >= 9:
+        print("The game is a draw")
+    else:
+        print_game()
+
+
 def add_user_positions(player_num, position):
     if player_num == "1":
         player1_positions.append(position)
     else:
         player2_positions.append(position)
+
+    check_for_winner(position)
 
 
 def user_input():
@@ -37,18 +57,20 @@ def user_input():
 
     pos = input(
         f"\nPlayer {player}:\nenter the position of the element you want to add \ni.e. top left is 0, 0, bottom right is 2, 2, bottom middle is 1, 2\n")
+
+    if pos in all_chosen:
+        print("Position taken, choose a different spot")
+        return user_input()
+
     try:
         place_in_pos = positions_dict[pos]
         Game[place_in_pos] = symbol
-        add_user_positions(player, pos)
         player1_turn = not player1_turn
-        print_game()
+        add_user_positions(player, pos)
 
     except KeyError:
         print("The value entered must be in this format: 0, 1")
         user_input()
-
-
 
 
 def print_game():
